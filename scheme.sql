02567c9b18c60ca49a1ee7fc2808027c8c8f7ca4
Moe <moha1058@gmail.com>
2020-07-16 20:21:13 +0100

    Set up databse

---
+++ scheme.sql
@@ -0,0 +1,17 @@
+CREATE TABLE users (
+    uid SERIAL PRIMARY KEY NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255)  NOT NULL UNIQUE,
+    hash_password TEXT NOT NULL,
+    birth_day TEXT NOT NULL,
+    gender VARCHAR(100),
+    joined TIMESTAMP
+);
+
+CREATE TABLE login (
+    uid SERIAL PRIMARY KEY NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    hash_password TEXT NOT NULL
+
+);
--- server.js
+++ server.js
@@ -5,8 +5,18 @@ const app = express();
 
 const port = 3000;
 
+
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
-app.get('/', (req, res) => res.send('server is working'));
+app.get('/', (req, res) => {
+        db.select('*').from('users')
+            .then(data => res.json(data));
+})
+
+app.post('/signin', (req, res) => {
+    
+})
+    
+
 app.listen(port, () => console.log(`Server is running on port ${port}`))
